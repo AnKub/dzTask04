ee54af8bc0b680b983ccdd69a33558ac57ac2b2f
AnKub <ankub3088@gmail.com>
2026-06-16 11:54:22 +0300

    UpdateReadme

--- src/mock/orders.ts
+++ src/mock/orders.ts
@@ -1,19 +1,19 @@
-// import { Order } from '../types/order';
+import { Order } from '../types/order';
 
-// export const orders: Order[] = [
-//   {
-//     id: 'order-1',
-//     name: 'Поставка моніторів Samsung',
-//     date: '2026-04-20',
-//   },
-//   {
-//     id: 'order-2',
-//     name: 'Поставка ноутбуків HP',
-//     date: '2026-04-18',
-//   },
-//   {
-//     id: 'order-3',
-//     name: 'Поставка клавіатур',
-//     date: '2026-04-15',
-//   },
-// ];
+export const orders: Order[] = [
+  {
+    id: 'order-1',
+    name: 'Поставка моніторів Samsung',
+    date: '2026-04-20',
+  },
+  {
+    id: 'order-2',
+    name: 'Поставка ноутбуків HP',
+    date: '2026-04-18',
+  },
+  {
+    id: 'order-3',
+    name: 'Поставка клавіатур',
+    date: '2026-04-15',
+  },
+];
