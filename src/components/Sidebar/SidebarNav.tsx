2ac789e3e55e2f665dc92ec4156299b69f98a0ad
AnKub <ankub3088@gmail.com>
2026-06-21 12:36:03 +0300

    optimazeHeader

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -1,29 +1,29 @@
-// import React from 'react';
-// import { useTranslation } from 'react-i18next';
-// import SidebarNavItem from './SidebarNavItem';
-// import './SidebarNav.scss';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import SidebarNavItem from './SidebarNavItem';
+import './SidebarNav.scss';
 
-// type SidebarNavProps = {
-//   onNavigate?: () => void;
-// };
+type SidebarNavProps = {
+  onNavigate?: () => void;
+};
 
-// const SidebarNav: React.FC<SidebarNavProps> = ({ onNavigate }) => {
-//   const { t } = useTranslation();
-//   const navItems = [
-//     { label: t('nav.orders'), path: '/orders' },
-//     { label: t('nav.groups'), path: '/groups' },
-//     { label: t('nav.products'), path: '/products' },
-//     { label: t('nav.users'), path: '/users' },
-//     { label: t('nav.settings'), path: '/settings' },
-//   ];
+const SidebarNav: React.FC<SidebarNavProps> = ({ onNavigate }) => {
+  const { t } = useTranslation();
+  const navItems = [
+    { label: t('nav.orders'), path: '/orders' },
+    { label: t('nav.groups'), path: '/groups' },
+    { label: t('nav.products'), path: '/products' },
+    { label: t('nav.users'), path: '/users' },
+    { label: t('nav.settings'), path: '/settings' },
+  ];
 
-//   return (
-//     <nav className="sidebar-nav">
-//       {navItems.map((item) => (
-//         <SidebarNavItem key={item.path} label={item.label} path={item.path} onNavigate={onNavigate} />
-//       ))}
-//     </nav>
-//   );
-// };
+  return (
+    <nav className="sidebar-nav">
+      {navItems.map((item) => (
+        <SidebarNavItem key={item.path} label={item.label} path={item.path} onNavigate={onNavigate} />
+      ))}
+    </nav>
+  );
+};
 
-// export default SidebarNav;
+export default SidebarNav;
