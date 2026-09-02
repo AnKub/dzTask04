d1378dd824b931e2ed975506ab8e2012c3f48324
AnKub <ankub3088@gmail.com>
2026-09-02 19:10:30 +0300

    BaseUserPage+sliceuser

--- src/services/inventoryDataService.ts
+++ src/services/inventoryDataService.ts
@@ -1,34 +1,34 @@
-// import { groups } from '../mock/groups';
-// import { orders } from '../mock/orders';
-// import { products } from '../mock/products';
-// import { Group } from '../types/group';
-// import { Order } from '../types/order';
-// import { Product } from '../types/product';
+import { groups } from '../mock/groups';
+import { orders } from '../mock/orders';
+import { products } from '../mock/products';
+import { Group } from '../types/group';
+import { Order } from '../types/order';
+import { Product } from '../types/product';
 
-// const cloneOrders = (): Order[] => orders.map((order) => ({ ...order }));
-// const cloneGroups = (): Group[] => groups.map((group) => ({ ...group }));
-// const cloneProducts = (): Product[] => products.map((product) => ({ ...product }));
+const cloneOrders = (): Order[] => orders.map((order) => ({ ...order }));
+const cloneGroups = (): Group[] => groups.map((group) => ({ ...group }));
+const cloneProducts = (): Product[] => products.map((product) => ({ ...product }));
 
-// export const inventoryDataService = {
-//   getOrders(): Order[] {
-//     return cloneOrders();
-//   },
+export const inventoryDataService = {
+  getOrders(): Order[] {
+    return cloneOrders();
+  },
 
-//   getGroups(): Group[] {
-//     return cloneGroups();
-//   },
+  getGroups(): Group[] {
+    return cloneGroups();
+  },
 
-//   getProducts(): Product[] {
-//     return cloneProducts();
-//   },
+  getProducts(): Product[] {
+    return cloneProducts();
+  },
 
-//   getSnapshot() {
-//     return {
-//       orders: cloneOrders(),
-//       groups: cloneGroups(),
-//       products: cloneProducts(),
-//     };
-//   },
-// };
+  getSnapshot() {
+    return {
+      orders: cloneOrders(),
+      groups: cloneGroups(),
+      products: cloneProducts(),
+    };
+  },
+};
 
-// export default inventoryDataService;
+export default inventoryDataService;
