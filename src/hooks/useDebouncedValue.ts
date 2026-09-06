7236984de53a3b86d4707e2dc3ccd95b04237dfd
AnKub <ankub3088@gmail.com>
2026-09-06 21:32:01 +0300

    BaseUserPage+sliceuser

--- src/hooks/useDebouncedValue.ts
+++ src/hooks/useDebouncedValue.ts
@@ -1,19 +1,19 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-const useDebouncedValue = <T,>(value: T, delay = 300) => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+// const useDebouncedValue = <T,>(value: T, delay = 300) => {
+//   const [debouncedValue, setDebouncedValue] = useState(value);
 
-  useEffect(() => {
-    const timeoutId = window.setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
+//   useEffect(() => {
+//     const timeoutId = window.setTimeout(() => {
+//       setDebouncedValue(value);
+//     }, delay);
 
-    return () => {
-      window.clearTimeout(timeoutId);
-    };
-  }, [delay, value]);
+//     return () => {
+//       window.clearTimeout(timeoutId);
+//     };
+//   }, [delay, value]);
 
-  return debouncedValue;
-};
+//   return debouncedValue;
+// };
 
-export default useDebouncedValue;
+// export default useDebouncedValue;
