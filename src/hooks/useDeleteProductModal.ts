1ffa042f6f3e0896267823c7b5df47b2fef89d68
AnKub <ankub3088@gmail.com>
2026-09-07 16:20:09 +0300

    BaseUserPage+sliceuser

--- src/hooks/useDeleteProductModal.ts
+++ src/hooks/useDeleteProductModal.ts
@@ -1,43 +1,43 @@
-// import { useMemo, useState } from 'react';
-// import { useTranslation } from 'react-i18next';
-// import { removeProduct } from '../store/productsSlice';
-// import { useAppDispatch } from '../store/hooks';
-// import { Product } from '../types/product';
+import { useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { removeProduct } from '../store/productsSlice';
+import { useAppDispatch } from '../store/hooks';
+import { Product } from '../types/product';
 
-// const useDeleteProductModal = (products: Product[]) => {
-//   const dispatch = useAppDispatch();
-//   const { t } = useTranslation();
-//   const [deleteId, setDeleteId] = useState<string | null>(null);
+const useDeleteProductModal = (products: Product[]) => {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+  const [deleteId, setDeleteId] = useState<string | null>(null);
 
-//   const deleteProduct = useMemo(
-//     () => products.find((product) => product.id === deleteId) ?? null,
-//     [deleteId, products]
-//   );
+  const deleteProduct = useMemo(
+    () => products.find((product) => product.id === deleteId) ?? null,
+    [deleteId, products]
+  );
 
-//   const requestDelete = (id: string) => {
-//     setDeleteId(id);
-//   };
+  const requestDelete = (id: string) => {
+    setDeleteId(id);
+  };
 
-//   const closeDeleteModal = () => {
-//     setDeleteId(null);
-//   };
+  const closeDeleteModal = () => {
+    setDeleteId(null);
+  };
 
-//   const confirmDelete = () => {
-//     if (deleteId) {
-//       dispatch(removeProduct(deleteId));
-//     }
+  const confirmDelete = () => {
+    if (deleteId) {
+      dispatch(removeProduct(deleteId));
+    }
 
-//     setDeleteId(null);
-//   };
+    setDeleteId(null);
+  };
 
-//   return {
-//     deleteId,
-//     deleteProduct,
-//     requestDelete,
-//     closeDeleteModal,
-//     confirmDelete,
-//     confirmText: deleteProduct ? t('modal.deleteProductPrompt', { name: deleteProduct.name }) : undefined,
-//   };
-// };
+  return {
+    deleteId,
+    deleteProduct,
+    requestDelete,
+    closeDeleteModal,
+    confirmDelete,
+    confirmText: deleteProduct ? t('modal.deleteProductPrompt', { name: deleteProduct.name }) : undefined,
+  };
+};
 
-// export default useDeleteProductModal;
+export default useDeleteProductModal;
