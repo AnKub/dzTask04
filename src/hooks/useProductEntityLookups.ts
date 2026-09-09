052091cd787c0b78d02f0298dfdd30c890976294
AnKub <ankub3088@gmail.com>
2026-09-09 12:48:48 +0300

    BaseUserPage+sliceuser

--- src/hooks/useProductEntityLookups.ts
+++ src/hooks/useProductEntityLookups.ts
@@ -1,28 +1,28 @@
-import { useMemo } from 'react';
-import { Group } from '../types/group';
-import { Order } from '../types/order';
+// import { useMemo } from 'react';
+// import { Group } from '../types/group';
+// import { Order } from '../types/order';
 
-const useProductEntityLookups = (groups: Group[], orders: Order[]) => {
-  const groupNames = useMemo(
-    () => new Map(groups.map((group) => [group.id, group.name])),
-    [groups]
-  );
+// const useProductEntityLookups = (groups: Group[], orders: Order[]) => {
+//   const groupNames = useMemo(
+//     () => new Map(groups.map((group) => [group.id, group.name])),
+//     [groups]
+//   );
 
-  const orderNames = useMemo(
-    () => new Map(orders.map((order) => [order.id, order.name])),
-    [orders]
-  );
+//   const orderNames = useMemo(
+//     () => new Map(orders.map((order) => [order.id, order.name])),
+//     [orders]
+//   );
 
-  const orderDates = useMemo(
-    () => new Map(orders.map((order) => [order.id, order.date])),
-    [orders]
-  );
+//   const orderDates = useMemo(
+//     () => new Map(orders.map((order) => [order.id, order.date])),
+//     [orders]
+//   );
 
-  return {
-    groupNames,
-    orderNames,
-    orderDates,
-  };
-};
+//   return {
+//     groupNames,
+//     orderNames,
+//     orderDates,
+//   };
+// };
 
-export default useProductEntityLookups;
+// export default useProductEntityLookups;
