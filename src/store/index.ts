ebc246881fc364f3a0b364a22406837f3810d74f
AnKub <ankub3088@gmail.com>
2026-09-17 17:03:51 +0300

    createUsersPage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import ordersReducer from './ordersSlice';
 import productsReducer from './productsSlice';
 import groupsReducer from './groupsSlice';
+import usersReducer from './usersSlice';
 
 export const store = configureStore({
   reducer: {
     orders: ordersReducer,
     products: productsReducer,
     groups: groupsReducer,
+    users: usersReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
