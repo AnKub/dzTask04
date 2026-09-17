5b386107a8551c75cf6b3d211454b41d47e61f62
AnKub <ankub3088@gmail.com>
2026-09-17 19:18:43 +0300

    createUsersPage

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { users } from '../../mock/users';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { setUsers } from '../../store/usersSlice';
+import './Users.scss';
 
-const Users: React.FC = () => {
-	const { t } = useTranslation();
+const Users: React.FC =()=> {
+const dispatch = useAppDispatch();
+const usersList = useAppSelector((state) => state.users.items);
+const { t } = useTranslation();
 
-	return <div>{t('users.title')}</div>;
+	useEffect(() => {
+		dispatch(setUsers(users));
+	}, [dispatch]);
+
+return (
+  <section className="users-page">
+    <div className="users-page__header">
+      <h1>{t('users.title')}</h1>
+      <span>{usersList.length}</span>
+    </div>
+
+    <div className="users-page__list">
+      {usersList.map((user) => (
+        <article className="users-page__item" key={user.id}>
+          <div className="users-page__avatar">
+            {user.name.charAt(0)}
+          </div>
+
+          <div className="users-page__identity">
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+
+          <div className="users-page__meta">
+            <span>{user.role}</span>
+            <span>{user.status}</span>
+          </div>
+        </article>
+      ))}
+    </div>
+  </section>
+);
 };
 
 export default Users;
